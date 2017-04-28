d26a2f5bd37c9247a41733c0a74c675bc5b797dc
Juan Zalles <juan.zalles@gmail.com>
2017-04-27 20:20:34 -0500

    last changes(mat)

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -16,13 +16,23 @@ end
 get '/comments/:id' do
   @comment = Comment.find(params[:id])
 
-  erb :'comments/comment'
+  erb :'comments/comment', locals: {comment: @comment}
 
 end
 get '/comments/:id/edit' do
   @comment = Comment.find(params[:id])
 
   erb :'comments/edit'
+
+end
+get '/comments/:id/upvote' do
+  @comment = Comment.find(params[:id])
+
+  @vote = Vote.find_or_create_by(votable_id: @comment.id)
+  @vote.weight=1
+  @vote.save
+
+  erb :'comments/comment', locals: {comment: @comment}
 
 end
 post '/comments/:id/edit' do
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,9 @@ class Comment < ActiveRecord::Base
   belongs_to :author, class_name: :User
   belongs_to :commentable, polymorphic: true
   has_many   :votes, as: :votable
+  def vote_total
+    total=0
+    self.votes.each {|v| total+=v.weight}
+    total
+  end
 end
