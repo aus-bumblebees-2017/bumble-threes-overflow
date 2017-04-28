f4c597065125829aa363fb96e48d16b4f797a392
Karina Franco <karinafraest@gmail.com>
2017-04-27 21:31:49 -0500

    seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,11 +15,50 @@ questions = []
 30.times do
 
   author = users.sample
-  title = Faker::Lorem.sentence + "?"
+  title = Faker::Lorem.sentence.chop + "?"
   description = Faker::Lorem.paragraph
 
-  questions << User.create(title: title, description: description, author: author)
+  questions << Question.create(title: title, description: description, author: author)
 end
+
+answers = []
+
+50.times do
+  author = users.sample
+  question = questions.sample
+  description = Faker::Lorem.paragraph
+
+  answers << Answer.create(description: description, author: author, question: question)
+
+end
+
+my_comments = []
+
+70.times do
+  author = users.sample
+  description = Faker::Lorem.paragraph
+  vesels = questions + answers
+  parent = vesels.sample
+  comment = Comment.create(description: description, author: author)
+  parent.comments << comment
+  my_comments << comment
+  parent.save
+end
+
+200.times do 
+  author = users.sample
+  vesels = questions+ answers + my_comments
+  parent = vesels.sample
+
+  parent.votes << Vote.create(author: author, weight: 1)
+  parent.save
+end
+
+
+
+
+
+
 
 
 
